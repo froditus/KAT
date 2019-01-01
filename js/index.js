8a91f14b20385549cbd8b3656083d1cd5be204d7
froditus <froditus@gmail.com>
2019-01-01 23:50:47 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 onDeviceReady: function() {
         app.receivedEvent('deviceready');
           window.open = cordova.InAppBrowser.open;
-            cordova.InAppBrowser.open('http://apache.org', '_self', 'location=yes');
+            //cordova.InAppBrowser.open('http://apache.org', '_self', 'location=yes');
+            cordova.InAppBrowser.open('https://apps.cylpro.com/user', '_system', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
     },
